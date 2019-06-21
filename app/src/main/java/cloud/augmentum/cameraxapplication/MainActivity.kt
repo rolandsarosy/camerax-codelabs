1ac8932aeb8daf95ec3b47e5bfe27fcd7988121f
Roland Sárosy <roland.sarosy@gmail.com>
2019-06-21 14:55:54 +0200

    Initial commit

---
+++ app/src/main/java/cloud/augmentum/cameraxapplication/MainActivity.kt
@@ -0,0 +1,143 @@
+package cloud.augmentum.cameraxapplication
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.os.Bundle
+import android.util.Log
+import android.util.Rational
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import java.io.File
+
+class MainActivity : AppCompatActivity(), LifecycleOwner {
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    private lateinit var viewFinder: TextureView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewFinder = findViewById(R.id.view_finder)
+
+        if (allPermissionsGranted()) {
+            viewFinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+    }
+
+
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetAspectRatio(Rational(1, 1))
+            setTargetResolution(Size(640, 640))
+            setLensFacing(CameraX.LensFacing.BACK)
+        }.build()
+
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        val imageCaptureConfig = ImageCaptureConfig.Builder().apply {
+            setTargetAspectRatio(Rational(1, 1))
+            setFlashMode(FlashMode.AUTO)
+            setLensFacing(CameraX.LensFacing.BACK)
+            setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+        }.build()
+
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        findViewById<Button>(R.id.action_button).setOnClickListener {
+            val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+            imageCapture.takePicture(file,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onError(useCaseError: ImageCapture.UseCaseError, message: String, cause: Throwable?) {
+                        val msg = "Photo capture failed: $message"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        Log.e("CameraXApp", msg)
+                        cause?.printStackTrace()
+                    }
+
+                    override fun onImageSaved(file: File) {
+                        val msg = "Photo capture succeeded: ${file.absolutePath}"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        Log.d("CameraXApp", msg)
+                        findViewById<ImageView>(R.id.image_view).setImageBitmap(getBitmapFromFile(file.absolutePath))
+                    }
+                })
+        }
+
+        CameraX.bindToLifecycle(this, preview, imageCapture)
+    }
+
+    // Compensate for changes in device orientation
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        val rotationDegrees = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+        viewFinder.setTransform(matrix)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getBitmapFromFile(path: String): Bitmap {
+        val options = BitmapFactory.Options()
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888
+        return BitmapFactory.decodeFile(path, options)
+    }
+
+    override fun getLifecycle(): Lifecycle {
+        return super.getLifecycle()
+    }
+}
